db10d3b1e3ba45ac57a23a27a07b24203dba0f7f
Agustín Córdoba <agustincordoba28@gmail.com>
2022-08-04 10:12:09 -0300

    test api sv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'entornos-spa';
+
+export class AppComponent implements OnInit{
+
+	constructor(private http: HttpClient) {
+	}
+
+    ngOnInit(): void {
+		this.http.get(`${environment.apiEndpoint}/users`,{
+		}).subscribe((response:any)=>{
+			console.log(response); 
+		});
+    }
+  
 }
