3ee5e7bb39d547d9b160ec9e32833a8ca170dc8b
Agustín Córdoba <agustincordoba28@gmail.com>
2022-08-06 14:36:58 -0300

    cambio de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,17 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/*
 export const environment = {
   production: false,
   apiEndpoint : 'http://api.entornos-frro.tk:8080/api',
 };
-*/
 
+/*
 export const environment = {
   production: false,
   apiEndpoint : 'http://localhost:8080/api',
 };
+*/
 
 /*
  * For easier debugging in development mode, you can import the following file
